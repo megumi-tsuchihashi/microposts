06b8755c5e7db2f19cffb8f8ed7908ce08486905
jiro.kirameki <ec2-user@ip-172-31-16-152.us-east-2.compute.internal>
2018-06-08 03:47:22 +0000

    follow, unfollow

--- app/Http/Controllers/UserFollowController.php
+++ app/Http/Controllers/UserFollowController.php
@@ -6,14 +6,12 @@ use Illuminate\Http\Request;
 
 class UserFollowController extends Controller
 {
-    public function store(Request $request, $id)
-    {
+    public function store(Request $request, $id){
         \Auth::user()->follow($id);
         return redirect()->back();
     }
 
-    public function destroy($id)
-    {
+    public function destroy($id){
         \Auth::user()->unfollow($id);
         return redirect()->back();
     }
--- app/User.php
+++ app/User.php
@@ -17,12 +17,13 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
-
-    /**
+    
+    /*
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
+     
     protected $hidden = [
         'password', 'remember_token',
     ];
@@ -80,4 +81,12 @@ class User extends Authenticatable
     public function is_following($userId) {
         return $this->followings()->where('follow_id', $userId)->exists();
     }
+    
+    public function feed_microposts()
+    {
+        $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return Micropost::whereIn('user_id', $follow_user_ids);
+    }
+    
 }
